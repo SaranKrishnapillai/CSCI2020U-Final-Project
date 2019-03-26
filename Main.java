6cbe2670a170d612b1441820752ad636db7ae06c
SaranKrishnapillai <46762925+SaranKrishnapillai@users.noreply.github.com>
2019-03-26 01:48:55 -0400

    After adding sockets

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -17,40 +19,117 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.scene.control.ListView;
-import java.io.File;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Main extends Application {
 
     Stage window;
     Scene scene1,scene2;
+
+    private Socket socket            = null;
+    private DataInputStream  input   = null;
+    private DataOutputStream out     = null;
+
+    public String address = "localhost";
+    public static int port = 1700;
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+
 
         window = primaryStage;
         Game_Content g = new Game_Content();
         Pane pane = new Pane();
 
-        Label lbl = new Label("Game Title");
-        lbl.setStyle(("-fx-text-fill: yellow; -fx-font: 50 'Star Jedi Hollow' "));
-        lbl.setTranslateX(145);
+        Label lbl = new Label("Space Blaster 3000");
+        lbl.setStyle(("-fx-text-fill: yellow; -fx-font: 45 'Star Jedi Hollow' "));
+        lbl.setTranslateX(55);
         lbl.setTranslateY(10);
 
         Button button = new Button("Play");
         button.setStyle("-fx-background-color: white; -fx-font: 25 'Star Jedi'; -fx-text-fill: deepskyblue;");
         button.setTranslateX(125);
         button.setTranslateY(120);
+        TextField userName = new TextField("Enter Username Here");
+
+        userName.setTranslateX(125);
+        userName.setTranslateY(180);
+
+
         button.setOnAction(new EventHandler<ActionEvent>() {
+            String userName1 = userName.getText();
+
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Starts Game");
+
+                try
+                {
+                    socket = new Socket(address, port);
+                    System.out.println("Connected");
+
+
+                    // sends output to the socket
+                    out    = new DataOutputStream(socket.getOutputStream());
+                }catch(UnknownHostException u)
+                {
+                    System.out.println(u);
+                }
+                catch(IOException i) {
+                    System.out.println(i);
+                }
+
+                // string to read message from input
+                String line = "!";
+                String userName1 = userName.getText();
+
+                // keep reading until "Over" is input
+                while (line.equals("!"))
+                {
+                    try
+                    {
+                        //line = input.readLine();
+                        line = userName1;
+                        out.writeUTF(line);
+                    }
+                    catch(IOException i)
+                    {
+                        System.out.println(i);
+                    }
+                }
+
+                // close the connection
+                try
+                {
+
+                    out.close();
+                    socket.close();
+                }
+                catch(IOException i)
+                {
+                    System.out.println(i);
+                }
+
+
                 window.setScene(scene2);
                 scene2.setOnKeyPressed(e -> g.keyPressed(e));
                 scene2.setOnKeyReleased(e -> g.keyReleased(e));
-
+                System.out.println();
             }
         });
+
+
         button.addEventHandler(MouseEvent.MOUSE_ENTERED,
                 new EventHandler<MouseEvent>() {
                     @Override
@@ -139,8 +218,16 @@ public class Main extends Application {
         button4.setStyle("-fx-background-color: white; -fx-font: 20 'Star Jedi'; -fx-text-fill: darkviolet;");
         button4.setTranslateX(355);
         button4.setTranslateY(125);
-        Score test = new Score("ass",10);
-        ListView<String> list1 = new ListView<String>();
+
+        Scanner s = new Scanner(new File("src/sample/leaderboards.txt"));
+        ObservableList<String> list = FXCollections.observableArrayList();
+        while (s.hasNext()){
+            list.add(s.next());
+        }
+        s.close();
+
+
+        ListView<String> list1 = new ListView<String>(list);
         list1.setPrefSize(110,240);
         list1.setTranslateX(355);
         list1.setTranslateY(200);
@@ -153,12 +240,12 @@ public class Main extends Application {
                 {
                     String a = "";
                     a += (top10[i].getName() + "          " + top10[i].getScore());
-                    list1.getItems().add(a);
+                    //list1.getItems().add(a);
                 }
             }
         });
         button4.addEventHandler(MouseEvent.MOUSE_ENTERED,
-                new EventHandler<>() {
+                new EventHandler<MouseEvent>() {
                     @Override
                     public void handle(MouseEvent e) {
                         button4.setStyle("-fx-background-color: darkviolet; -fx-font: 20 'Star Jedi'; -fx-text-fill: white;");
@@ -173,15 +260,17 @@ public class Main extends Application {
                 });
 
 
-        ImageView img = new ImageView("SpaceBackGround.jpg");
+        ImageView img = new ImageView("sample/SpaceBackGround.jpg");
         img.setFitHeight(500);
         img.setFitWidth(600);
 
 
 
+
+
         pane.getChildren().add(img);
         pane.getChildren().add(lbl);
-        pane.getChildren().addAll(button,button2,txt,button3,button4,list1);
+        pane.getChildren().addAll(button,button2,txt,button3,button4,list1,userName);
         scene2 = new Scene(g.getRoot(),800,600);
         scene1 = new Scene(pane,600,500);
         window.setTitle("Main Menu");
